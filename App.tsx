8f14b0beaf88abaec702fee869ea61a02685f7b7
Not4PplPrsn <ST10470237@myemeris.edu.za>
2026-08-12 08:37:38 +0200

    Fixed an error with one of the tags

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Image, TextInput,   } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  
+      <View style={styles.container}>
+        <Text style={styles.heading}>Name</Text>
+        <Text style={styles.heading}>Age</Text>
+        <Text style={styles.heading}>Slots </Text>
+        
+        <TextInput style={styles.texInputs} placeholder="Enter your name" />
+
+
+        <StatusBar style="auto" />
+      </View>
+
   );
 }
 
@@ -17,4 +26,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading:{fontSize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'Arial',
+    color: 'blue',
+  },
+  imageCotainer: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    overflow: 'hidden',
+    marginBottom: 20,
+    borderWidth: 4,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  texInputs: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,  
+  }
+
 });
