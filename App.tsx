c414d44f81a4f3bc17903c8c06207376f3815b03
Not4PplPrsn <ST10470237@myemeris.edu.za>
2026-08-16 19:06:01 +0200

    Added new styles and an input box

--- App.tsx
+++ App.tsx
@@ -6,8 +6,16 @@ import image from './assets/image.png';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image source={image} style={styles.image} />
+
+      <View >
+        <View style={styles.imageContainer}>
+          <Image source={image} style={styles.image} />
+        </View>
+
+        <Text style = {styles.agentInformation}>Full Name: Sipho Ndlovu </Text>
+        <Text style = {styles.agentInformation}>Years Experience: 99</Text>
+        <Text style = {styles.agentInformation}>Consultation fee: R999</Text>
+        <Text style = {styles.agentInformation}>Slots filled: 0</Text>
       </View>
       <TextInput style={styles.texInputs} placeholder="Enter your name" />
       <StatusBar style="auto" />
@@ -21,6 +29,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    height: 'auto',
+    width: 'auto',
   },
   heading:{fontSize: 20,
     fontWeight: 'bold',
@@ -53,6 +63,18 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingHorizontal: 10,  
     
+  },
+  agentInformation:{
+    fontSize: 14,
+    fontWeight: "100",
+    fontFamily: "Arial",
+    borderWidth:0,
+    backgroundColor: "rgb(10, 36, 107)",
+    width: '50%',
+    height: 'auto',
+    padding: 7, 
+    marginBottom: 5,
+
   }
 
 });
